142c79211951723be4ef05441ae53fc47e276e1d
Bohdan Shaban <bohdanshaban97@gmail.com>
2021-06-24 08:46:00 +0200

    Updates

---
+++ precache-manifest.418fbed767f98b62e15c890b8962cd1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42cd7fb54fb3a4ee2f1d",
+    "url": "/game_of_thrones-app/static/js/runtime~main.42cd7fb5.js"
+  },
+  {
+    "revision": "956409112214d7d7e74a",
+    "url": "/game_of_thrones-app/static/js/main.95640911.chunk.js"
+  },
+  {
+    "revision": "08d08a3826cb39c75928",
+    "url": "/game_of_thrones-app/static/js/1.08d08a38.chunk.js"
+  },
+  {
+    "revision": "956409112214d7d7e74a",
+    "url": "/game_of_thrones-app/static/css/main.081dbb05.chunk.css"
+  },
+  {
+    "revision": "08d08a3826cb39c75928",
+    "url": "/game_of_thrones-app/static/css/1.28b4ebc8.chunk.css"
+  },
+  {
+    "revision": "7742a17c5ce1a69b2e03426188402842",
+    "url": "/game_of_thrones-app/index.html"
+  }
+];
